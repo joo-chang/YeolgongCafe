6013778e083cf98f0d2163f5bbf31510836c89bb
박윤영 <dbsdud1025@naver.com>
2020-09-23 12:11:27 +0900

    page 분할

--- StudyCafe/src/StartPage2.java
+++ StudyCafe/src/StartPage2.java
@@ -49,87 +49,7 @@ public class StartPage2 {
 		frame.getContentPane().setLayout(null);
 			
 		JPanel panel = new JPanel(); //main
-		JPanel CreateAccountPanel = new JPanel(); //Account
 	
-		
-		//회원가입 gui
-		CreateAccountPanel.setBounds(0, 0, 784, 561);
-		frame.getContentPane().add(CreateAccountPanel);
-		CreateAccountPanel.setVisible(false);
-		CreateAccountPanel.setLayout(null);
-		
-		JLabel Accountname = new JLabel("이름");
-		Accountname.setBounds(262, 103, 111, 35);
-		frame.getContentPane().add(Accountname);
-		CreateAccountPanel.add(Accountname);
-		
-		JLabel AccountId = new JLabel("I D");
-		AccountId.setBounds(262, 169, 62, 18);
-		frame.getContentPane().add(AccountId);
-		CreateAccountPanel.add(AccountId);
-		
-		JLabel lblNewLabel_2 = new JLabel("Password");
-		lblNewLabel_2.setBounds(262, 229, 98, 18);
-		CreateAccountPanel.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("생년월일");
-		lblNewLabel_3.setBounds(262, 289, 62, 18);
-		CreateAccountPanel.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_4 = new JLabel("핸드폰 번호");
-		lblNewLabel_4.setBounds(262, 345, 98, 18);
-		CreateAccountPanel.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_5 = new JLabel("회원가입");
-		lblNewLabel_5.setFont(new Font("휴먼엑스포", Font.PLAIN, 30));
-		lblNewLabel_5.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_5.setBounds(303, 34, 168, 44);
-		CreateAccountPanel.add(lblNewLabel_5);
-		
-		JTextField textField_6 = new JTextField();
-		textField_6.setBounds(355, 108, 116, 24);
-		CreateAccountPanel.add(textField_6);
-		textField_6.setColumns(10);
-		
-		JTextField textField_7 = new JTextField();
-		textField_7.setBounds(355, 166, 116, 24);
-		CreateAccountPanel.add(textField_7);
-		textField_7.setColumns(10);
-		
-		JTextField textField_2 = new JTextField();
-		textField_2.setBounds(355, 226, 116, 24);
-		CreateAccountPanel.add(textField_2);
-		textField_2.setColumns(10);
-		
-		JTextField textField_3 = new JTextField();
-		textField_3.setBounds(355, 286, 116, 24);
-		CreateAccountPanel.add(textField_3);
-		textField_3.setColumns(10);
-		
-		JTextField textField_4 = new JTextField();
-		textField_4.setBounds(355, 342, 167, 24);
-		CreateAccountPanel.add(textField_4);
-		textField_4.setColumns(10);
-		
-		JButton btnNewButton_3 = new JButton("중복확인"); 
-		btnNewButton_3.setBounds(493, 165, 105, 27);
-		CreateAccountPanel.add(btnNewButton_3);
-		
-		JButton btnNewButton_1 = new JButton("가입하기");
-		btnNewButton_1.setBounds(417, 411, 105, 27);
-		CreateAccountPanel.add(btnNewButton_1);
-		
-		JButton btnNewButton_2 = new JButton("취소");
-		btnNewButton_2.setBounds(255, 411, 105, 27);
-		btnNewButton_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CreateAccountPanel.setVisible(false);
-				panel.setVisible(true);
-			
-			}
-		});
-		CreateAccountPanel.add(btnNewButton_2);
-		
 		//메인gui
 		panel.setBounds(0, 0, 784, 561);
 		frame.getContentPane().add(panel);
@@ -157,8 +77,7 @@ public class StartPage2 {
 		btnJoin.setFont(new Font("휴먼엑스포", Font.PLAIN, 16));
 		btnJoin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				panel.setVisible(false);
-				CreateAccountPanel.setVisible(true);
+				new CreateAccount();
 			}
 		});
 		panel.add(btnJoin);
