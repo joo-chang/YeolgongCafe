721f602f77779e0b57db6b15e0d221e7fe895ea2
dudqls1441 <dudqls1441@naver.com>
2020-09-28 17:25:20 +0900

    db연동

---
+++ StudyCafe/src/SEAT.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SEAT {
+	private int seatnumber;
+	public int getSeatnumber() {
+		return seatnumber;
+	}
+	public void setSeatnumber(int seatnumber) {
+		this.seatnumber = seatnumber;
+	}
+	public int getPosition() {
+		return position;
+	}
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	private int position;
+	
+BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
+	
+	public SEAT() throws IOException {
+		System.out.println("좌석번호");
+		seatnumber=in.read();
+		System.out.println("자석위치");
+		position=in.read();
+	
+	}
+}
