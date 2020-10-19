6f2ccef7f541869275066f4cfa985c689717fec5
joo-chang <jooc031@naver.com>
2020-10-19 17:07:27 +0900

    깃 충돌 테스트

--- StudyCafe/src/gui/ChatPage.java
+++ StudyCafe/src/gui/ChatPage.java
@@ -24,10 +24,12 @@ public class ChatPage {
 		JLabel user_lb = new JLabel("전체 접속자");
 		user_lb.setBounds(25, 31, 102, 18);
 		chatFrame.getContentPane().add(user_lb);
+		//test 충돌 테스트
 		
 		JList user_List = new JList();
 		user_List.setBounds(25, 53, 86, 139);
 		chatFrame.getContentPane().add(user_List);
+		//test 충돌 테스트
 		
 		JScrollPane scroll_pane = new JScrollPane();
 		scroll_pane.setBounds(125, 31, 486, 389);
@@ -37,6 +39,7 @@ public class ChatPage {
 		scroll_pane.setViewportView(chatting_tf);
 		chatting_tf.setColumns(10);
 		
+		//test 충돌 테스트
 		JTextField send_tf = new JTextField("메세지를 입력하세요");
 		send_tf.setBounds(125, 431, 386, 24);
 		chatFrame.getContentPane().add(send_tf);
@@ -45,5 +48,7 @@ public class ChatPage {
 		JButton btnsend = new JButton("전송");
 		btnsend.setBounds(518, 430, 92, 27);
 		chatFrame.getContentPane().add(btnsend);
+		
+		//test 충돌 테스트
 	}
 }
